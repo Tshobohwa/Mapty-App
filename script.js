52cc552557961fe82492c27959c3c014c383ac17
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-04-23 15:19:22 +0200

    Get the current position and log it to the console

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// getting the current position and loging it lo the console in a google map link
+navigator.geolocation.getCurrentPosition(
+  pos => {
+    const { longitude, latitude } = pos.coords;
+    console.log(longitude, latitude);
+    const googleMapsUrl = `https://www.google.com/maps/@${latitude},${longitude}z`;
+    console.log(googleMapsUrl);
+  },
+  () => {
+    alert('could not get your position');
+  }
+);
