3d9795c4c6e581cf299afdab3cf10f880f68ae89
Salomon Tshobohwa <stshobohwa@gmail.com>
2023-04-23 21:10:12 +0200

    Show the input form

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 // getting the current position and loging it lo the console in a google map link
 navigator.geolocation.getCurrentPosition(
   pos => {
@@ -19,32 +21,53 @@ navigator.geolocation.getCurrentPosition(
     const googleMapsUrl = `https://www.google.com/maps/@${latitude},${longitude}z`;
     console.log(googleMapsUrl);
     const coords = [latitude, longitude];
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    map.on('click', function (mapEvent) {
-      console.log(mapEvent);
-      const { lat, lng } = mapEvent.latlng;
-
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            minWidth: 100,
-            maxWidth: 250,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-        )
-        .openPopup();
+    map.on('click', function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
     });
   },
   () => {
     alert('could not get your position');
   }
 );
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // Clear inputs
+  inputCadence.value =
+    inputDistance.value =
+    inputDuration.value =
+    inputElevation.value =
+      '';
+
+  //Display marker
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        minWidth: 100,
+        maxWidth: 250,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+        textBack: '',
+      })
+    )
+    .openPopup();
+});
+
+inputType.addEventListener('change', () => {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
